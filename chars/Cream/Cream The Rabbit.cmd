10d641e8349ff6d2991a76c0bf52029e1c182117
malikMai <malikcreese@gmail.com>
2024-07-06 14:09:18 -0400

    Cream char file

---
+++ chars/Cream/Cream The Rabbit.cmd
@@ -0,0 +1,819 @@
+;===============================================================================
+;-------------------------------Comandos----------------------------------------
+;===============================================================================
+[Remap]
+x = x
+y = y
+z = z
+a = a
+b = b
+c = c
+s = s
+
+[Command]
+name = "Super Jump"
+Command = ~D,U
+time = 10
+
+;-| Specials |-------------------------------------------------------
+[Command]
+name = "SPECIAL 1"
+Command = ~D,DF,F,a
+time = 15
+
+[Command]
+name = "SPECIAL 2"
+Command = ~D,DB,B,a
+time = 15
+
+[Command]
+name = "SPECIAL 3"
+Command = ~D,DF,F,b
+time = 15
+
+[Command]
+name = "SPECIAL 4"
+Command = ~D,DB,B,b
+time = 15
+
+[Command]
+name = "SPECIAL 5"
+Command = ~D,DF,F,c
+time = 15
+
+[Command]
+name = "SPECIAL 6"
+Command = ~D,DB,B,c
+time = 15
+
+[Command]
+name = "SPECIAL 7"
+Command = z
+time = 15
+
+[Defaults]
+
+;-| Double Tap |-----------------------------------------------------------
+[Command]
+name = "FF"     ;Required (do not remove)
+Command = F, F
+time = 10
+
+[Command]
+name = "BB"     ;Required (do not remove)
+Command = B, B
+time = 10
+;-| 2/3 Button Combination |-----------------------------------------------
+[Command]
+name = "recovery" ;Required (do not remove)
+Command = x+y
+time = 1
+
+[Command]
+name = "recovery"
+Command = y+z
+time = 1
+
+[Command]
+name = "recovery"
+Command = x+z
+time = 1
+
+[Command]
+name = "recovery"
+Command = a+b
+time = 1
+
+[Command]
+name = "recovery"
+Command = b+c
+time = 1
+
+[Command]
+name = "recovery"
+Command = a+c
+time = 1
+;-| Dir + Button |---------------------------------------------------------
+[Command]
+name = "back_x"
+Command = /$B,x
+time = 1
+
+[Command]
+name = "back_y"
+Command = /$B,y
+time = 1
+
+[Command]
+name = "back_z"
+Command = /$B,z
+time = 1
+
+[Command]
+name = "down_x"
+Command = /$D,x
+time = 1
+
+[Command]
+name = "down_y"
+Command = /$D,y
+time = 1
+
+[Command]
+name = "down_z"
+Command = /$D,z
+time = 1
+
+[Command]
+name = "fwd_x"
+Command = /$F,x
+time = 1
+
+[Command]
+name = "fwd_y"
+Command = /$F,y
+time = 1
+
+[Command]
+name = "fwd_z"
+Command = /$F,z
+time = 1
+
+[Command]
+name = "up_x"
+Command = /$U,x
+time = 1
+
+[Command]
+name = "up_y"
+Command = /$U,y
+time = 1
+
+[Command]
+name = "up_z"
+Command = /$U,z
+time = 1
+
+[Command]
+name = "back_a"
+Command = /$B,a
+time = 1
+
+[Command]
+name = "back_b"
+Command = /$B,b
+time = 1
+
+[Command]
+name = "back_c"
+Command = /$B,c
+time = 1
+
+[Command]
+name = "down_a"
+Command = /$D,a
+time = 1
+
+[Command]
+name = "down_b"
+Command = /$D,b
+time = 1
+
+[Command]
+name = "down_c"
+Command = /$D,c
+time = 1
+
+[Command]
+name = "fwd_a"
+Command = /$F,a
+time = 1
+
+[Command]
+name = "fwd_b"
+Command = /$F,b
+time = 1
+
+[Command]
+name = "fwd_c"
+Command = /$F,c
+time = 1
+
+[Command]
+name = "up_a"
+Command = /$U,a
+time = 1
+
+[Command]
+name = "up_b"
+Command = /$U,b
+time = 1
+
+[Command]
+name = "up_c"
+Command = /$U,c
+time = 1
+;-| Single Button |---------------------------------------------------------
+[Command]
+name = "a"
+Command = a
+time = 1
+
+[Command]
+name = "b"
+Command = b
+time = 1
+
+[Command]
+name = "c"
+Command = c
+time = 1
+
+[Command]
+name = "x"
+Command = x
+time = 1
+
+[Command]
+name = "y"
+Command = y
+time = 1
+
+[Command]
+name = "z"
+Command = z
+time = 1
+
+[Command]
+name = "s"
+Command = s
+time = 1
+;-| Single Dir |------------------------------------------------------------
+[Command]
+name = "fwd" ;Required (do not remove)
+Command = $F
+time = 1
+
+[Command]
+name = "downfwd"
+Command = $DF
+time = 1
+
+[Command]
+name = "down" ;Required (do not remove)
+Command = $D
+time = 1
+
+[Command]
+name = "downback"
+Command = $DB
+time = 1
+
+[Command]
+name = "back" ;Required (do not remove)
+Command = $B
+time = 1
+
+[Command]
+name = "upback"
+Command = $UB
+time = 1
+
+[Command]
+name = "up" ;Required (do not remove)
+Command = $U
+time = 1
+
+[Command]
+name = "upfwd"
+Command = $UF
+time = 1
+;-| Hold Button |--------------------------------------------------------------
+[Command]
+name = "hold_x"
+Command = /x
+time = 1
+
+[Command]
+name = "hold_y"
+Command = /y
+time = 1
+
+[Command]
+name = "hold_z"
+Command = /z
+time = 1
+
+[Command]
+name = "hold_a"
+Command = /a
+time = 1
+
+[Command]
+name = "hold_b"
+Command = /b
+time = 1
+
+[Command]
+name = "hold_c"
+Command = /c
+time = 1
+
+[Command]
+name = "hold_s"
+Command = /s
+time = 1
+;-| Hold Dir |--------------------------------------------------------------
+[Command]
+name = "holdfwd" ;Required (do not remove)
+Command = /$F
+time = 1
+
+[Command]
+name = "holddownfwd"
+Command = /$DF
+time = 1
+
+[Command]
+name = "holddown" ;Required (do not remove)
+Command = /$D
+time = 1
+
+[Command]
+name = "holddownback"
+Command = /$DB
+time = 1
+
+[Command]
+name = "holdback" ;Required (do not remove)
+Command = /$B
+time = 1
+
+[Command]
+name = "holdupback"
+Command = /$UB
+time = 1
+
+[Command]
+name = "holdup" ;Required (do not remove)
+Command = /$U
+time = 1
+
+[Command]
+name = "holdupfwd"
+Command = /$UF
+time = 1
+;---------- Comandos de la Inteligencia Artificial-----------------
+; Comandos activadores
+[Command]
+name = "AI Prueba"
+Command = y+z ; con el botï¿½n A+B activamos la AI
+time = 1
+
+[Command]
+name = "AI desactivado"
+Command = b+c ; Con el boton B+C desactivamos la AI
+time = 1
+;---------------------------------------------------------------------------
+[Statedef -1]
+;===========================================================================
+;---------------------------Basicos-----------------------------------------
+;---------------------------------------------------------------------------
+; Correr Adelante
+[State -1, Correr Adelante]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 60
+triggerall = Stateno != 70
+value = 60
+trigger1 = Command = "FF"
+trigger1 = StateType != A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Atras
+[State -1, Correr Atras]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 60
+triggerall = Stateno != 70
+value = 70
+trigger1 = Command = "BB"
+trigger1 = StateType != A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Adelante (Aire)
+[State -1, Correr Adelante (Aire)]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 62
+triggerall = Stateno != 72
+value = 62
+triggerall = Command = "holdfwd"
+triggerall = Command = "s"
+trigger1 = StateType = A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Atras (Aire)
+[State -1, Correr Atras (Aire)]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 62
+triggerall = Stateno != 72
+value = 72
+triggerall = Command = "holdback"
+triggerall = Command = "s"
+trigger1 = StateType = A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Super Jump
+[State -1, Super Jump]
+Type = ChangeState
+TriggerALL = AiLevel = 0
+Value = 80
+Trigger1 = Command = "Super Jump"
+Trigger1 = StateType = S
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Air Dash Forward
+[State -1, Air Dash Forward]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 110
+triggerall = Stateno != 115
+value = 110
+trigger1 = Command = "FF"
+trigger1 = StateType = A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Air Dash Backwards
+[State -1, Air Dash Backwards]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = stateno != 110
+triggerall = stateno != 115
+value = 115
+trigger1 = Command = "BB"
+trigger1 = statetype = A
+trigger1 = ctrl
+;---------------------------------------------------------------------------
+; Correr Adelante
+[State -1, Correr Adelante]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 60
+triggerall = Stateno != 70
+value = 60
+trigger1 = command = "holdfwd"
+trigger1 = command = "x"
+trigger1 = StateType != A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Atras
+[State -1, Correr Atras]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 60
+triggerall = Stateno != 70
+value = 70
+trigger1 = command = "holdback"
+trigger1 = command = "x"
+trigger1 = StateType != A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Adelante (Aire)
+[State -1, Correr Adelante (Aire)]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 62
+triggerall = Stateno != 72
+value = 62
+trigger1 = command = "holdfwd"
+trigger1 = command = "x"
+trigger1 = StateType = A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Correr Atras (Aire)
+[State -1, Correr Atras (Aire)]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 62
+triggerall = Stateno != 72
+value = 72
+trigger1 = command = "holdback"
+trigger1 = command = "x"
+trigger1 = StateType = A
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Super Jump
+[State -1, Super Jump]
+Type = ChangeState
+TriggerALL = AiLevel = 0
+Value = 80
+Trigger1 = Command = "Super Jump"
+Trigger1 = StateType = S
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Air Dash Forward
+[State -1, Air Dash Forward]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = Stateno != 110
+triggerall = Stateno != 115
+value = 110
+trigger1 = command = "holdfwd"
+trigger1 = command = "x"
+trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Air Dash Backwards
+[State -1, Air Dash Backwards]
+type = ChangeState
+TriggerALL = AiLevel = 0
+triggerall = stateno != 110
+triggerall = stateno != 115
+value = 115
+trigger1 = command = "holdback"
+trigger1 = command = "x"
+trigger1 = statetype = A
+trigger1 = ctrl
+;===========================================================================
+;Ultimate:
+;===========================================================================
+
+;===========================================================================
+;SKILL:
+;===========================================================================
+;---------------------------------------------------------------------------
+; Cheese Cannon
+[State -1, Cheese Cannon]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1000
+Value = 1400
+Triggerall = Command = "SPECIAL 1"|| Command = "SPECIAL 5"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+
+; Cheese Cannon (Aire)
+[State -1, Cheese Cannon (Aire)]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1000
+Value = 1405
+Triggerall = Command = "SPECIAL 1"|| Command = "SPECIAL 5"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+;---------------------------------------------------------------------------
+; Chao Storm
+[State -1, Chao Storm]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1500
+Value = 1500
+Triggerall = Command = "SPECIAL 2"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+;---------------------------------------------------------------------------
+; Chao Charge
+[State -1, Chao Charge]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 500
+Value = 1000
+Triggerall = Command = "SPECIAL 3"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+
+; Chao Charge (Aire)
+[State -1, Chao Charge (Aire)]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 500
+Value = 1005
+Triggerall = Command = "SPECIAL 3"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+;---------------------------------------------------------------------------
+; Bunny Harmmer
+[State -1,Bunny Harmmer]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1000
+Value = 1200
+Triggerall = Command = "SPECIAL 4"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+;---------------------------------------------------------------------------
+; Chao Assault
+[State -1, Chao Assault]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 3000
+Value = 3000
+Triggerall = Command = "SPECIAL 6"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+;---------------------------------------------------------------------------
+; Bunny Counter
+[State -1, Bunny Counter]
+type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1000
+value = 1300
+triggerall = command = "y"
+Triggerall = StateType != A
+trigger1 = ctrl
+trigger2 = numhelper(900) > 0
+
+
+; Bunny Counter (Aire)
+[State -1,Bunny Counter (Aire)]
+Type = ChangeState
+triggerall = var(2) = 0
+Triggerall = power >= 1000
+Value = 1305
+triggerall = command = "y"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+
+;---------------------------------------------------------------------------
+; Chao Landmine
+[State -1,Down + C]
+type=ChangeState
+value = 470
+triggerall = var(2) = 0
+triggerall = numhelper(475) = 0
+triggerall = power >= 350
+Triggerall = Command = "holddown"
+triggerall = Command = "z"
+Triggerall = statetype != A
+trigger1 = ctrl
+;---------------------------------------------------------------------------
+; Chao Boot
+[State -1, Chao Boot]
+Type = ChangeState
+triggerall = var(2) = 0
+triggerall = numhelper(1150) = 0
+Triggerall = power >= 1000
+Value = 1100
+triggerall = Command = "z"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+Trigger2 = numhelper(900) > 0
+
+;---------------------------------------------------------------------------
+; Down + A
+[State -1, Down + A]
+Type = ChangeState
+Value = 280
+triggerall = var(2) = 0
+Triggerall = Command = "a"
+Triggerall = Command = "holddown"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Down + B
+[State -1, Down + B]
+Type = ChangeState
+Value = 380
+triggerall = var(2) = 0
+Triggerall = Command = "b"
+Triggerall = command = "holddown"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+
+;===========================================================================
+;--------------------------------Ataques Normales---------------------------
+;---------------------------------------------------------------------------
+; Down + A
+[State -1, Down + A]
+Type = ChangeState
+Value = 280
+triggerall = var(2) = 0
+Triggerall = Command = "a"
+Triggerall = Command = "holddown"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; A
+[State -1, A]
+Type = ChangeState
+Value = 200
+triggerall = var(2) = 0
+Triggerall = Command = "a"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Down + B
+[State -1, Down + B]
+Type = ChangeState
+Value = 380
+triggerall = var(2) = 0
+Triggerall = Command = "b"
+Triggerall = command = "holddown"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; B
+[State -1, B]
+Type = ChangeState
+Value = 300
+triggerall = var(2) = 0
+Triggerall = Command = "b"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Down + C
+;[State -1,Down + C]
+;type=ChangeState
+;value = 470
+;triggerall = var(2) = 0
+;triggerall = numhelper(475) = 0
+;triggerall = power >= 350
+;triggerall = Command = "c"
+;triggerall = Command = "holddown"
+;Triggerall = statetype != A
+;trigger1 = ctrl
+;---------------------------------------------------------------------------
+; C
+[State -1, C]
+Type = ChangeState
+Value = 400
+triggerall = var(2) = 0
+triggerall = numhelper(405) = 0
+Triggerall = power >= 250
+Triggerall = Command = "c"
+Triggerall = StateType != A
+Trigger1 = Ctrl
+
+;---------------------------------------------------------------------------
+; A Aire
+[State -1, A Aire]
+Type = ChangeState
+Value = 600
+triggerall = var(2) = 0
+Triggerall = Command = "a"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Down + B Aire
+[State -1, Down + B Aire]
+Type = ChangeState
+Value = 640
+triggerall = var(2) = 0
+Triggerall = Command = "b"
+Triggerall = command = "holddown"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; B Aire
+[State -1, B Aire]
+Type = ChangeState
+Value = 610
+triggerall = var(2) = 0
+Triggerall = Command = "b"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+;---------------------------------------------------------------------------
+; Down + C Aire
+[State -1,Down + C Aire]
+type=ChangeState
+value = 615
+triggerall = var(2) = 0
+triggerall = numhelper(645) = 0
+triggerall = power >= 250
+triggerall = Command = "c"
+triggerall = Command = "holddown"
+Triggerall = statetype = A
+trigger1 = ctrl
+;---------------------------------------------------------------------------
+; C Aire
+[State -1, C Aire]
+Type = ChangeState
+Value = 620
+triggerall = var(2) = 0
+triggerall = numhelper(625) = 0
+triggerall = power >= 250
+Triggerall = power >= 350
+Triggerall = Command = "c"
+Triggerall = StateType = A
+Trigger1 = Ctrl
+;===========================================================================
+; CODING FOR ALL VAR	
+;===========================================================================
+;---------------------------------------------------------------------------
+; Power Charge
+[State -1, Power Charge]
+type = ChangeState
+Triggerall = power < 3000
+value = 500
+triggerall = command = "s"
+Triggerall = statetype != A
+trigger1 = ctrl
